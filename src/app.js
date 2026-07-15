e252aaaa09ddb026753970137e7551cebea133f4
apple <apple@appleui-MacBookAir.local>
2026-07-15 18:51:05 +0900

    chore: package 추가 및 setting

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+// import routes from "./routes/index.js";
 
+import { notFoundMiddleware } from "./common/middleware/notFound.middleware.js";
+import { errorMiddleware } from "./common/middleware/error.middleware.js";
 import { swaggerServe, swaggerSetup } from "./config/swagger.js";
 
 dotenv.config();
@@ -17,6 +21,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api-docs", swaggerServe, swaggerSetup);
 
 app.get("/", (req, res) => {
@@ -25,6 +30,10 @@ app.get("/", (req, res) => {
     message: "Server is running",
   });
 });
+
+app.use(routes);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/lib/prisma.js
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+export const prisma = new PrismaClient();
+
+// 사용법
+// import { prisma } from '../../lib/prisma.js'
