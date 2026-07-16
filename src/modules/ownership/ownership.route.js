951f71e5842083132edcb597063d0cff57e9f932
apple <apple@appleui-MacBookAir.local>
2026-07-16 15:04:28 +0900

    feat: ownership 관련 BE 작업

---
+++ src/modules/ownership/ownership.route.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+import * as ownershipController from "./ownership.controller.js";
+import { authMiddleware } from "../../common/middleware/auth.middleware.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import {
+  validateParams,
+  validateQuery,
+} from "../../common/middleware/validate.middleware.js";
+import {
+  getMyOwnershipsQuerySchema,
+  ownershipIdParamSchema,
+} from "./ownership.validator.js";
+
+const router = express.Router();
+
+router.get(
+  "/me",
+  authMiddleware,
+  validateQuery(getMyOwnershipsQuerySchema),
+  asyncHandler(ownershipController.getMyOwnerships),
+);
+
+router.get(
+  "/:ownershipId",
+  authMiddleware,
+  validateParams(ownershipIdParamSchema),
+  asyncHandler(ownershipController.getOwnership),
+);
+
+export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
 import photocardRouter from "../modules/photocard/photocard.route.js";
+import ownershipRouter from "../modules/ownership/ownership.route.js";
 
 router.use("/photocards", photocardRouter);
+router.use("/ownerships", ownershipRouter);
