97003e50fe01934c9bd847c93d44156aee1f5a40
apple <apple@appleui-MacBookAir.local>
2026-07-24 09:33:42 +0900

    feat: 유저 생성 및 조회 DB 통신(Repository) 로직 추가

---
+++ src/modules/auth/auth.repository.js
@@ -0,0 +1,13 @@
+import { prisma, Prisma } from "../../lib/prisma.js";
+
+export async function findUserByEmail(email, client = prisma) {
+  return client.user.findUnique({
+    where: { email },
+  });  
+}
+
+export async function createUser(data, client = prisma) {
+  return client.user.create({
+    data,
+  });
+}
