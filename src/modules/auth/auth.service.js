6461e99800677238125e1ecd1c3b0ca19ce0c2ad
apple <apple@appleui-MacBookAir.local>
2026-07-24 10:31:58 +0900

    feat: 회원가입 비지니스 로직(Service) 구현 및 비밀번호 암호화 적용

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcrypt";
+import * as authRepository from "./auth.repository.js";
+import { AppError } from "../../common/errors/AppError.js";
+import { ERROR_CODE } from "../../common/errors/errorCode.js";
+
+export async function signup({ email, nickname, password }) {
+  const existingUser = await authRepository.findUserByEmail(email);
+  if (existingUser) {
+    throw new AppError("이미 존재하는 이메일입니다.", 409, ERROR_CODE.CONFLICT);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await authRepository.createUser({
+    email,
+    nickname,
+    password: hashedPassword,
+  });
+
+  const { password: _, ...userWithoutPassword } = newUser;
+  return userWithoutPassword;
+}
