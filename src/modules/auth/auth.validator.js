53b32b489230322181215f87c8b607ff99602a09
apple <apple@appleui-MacBookAir.local>
2026-07-24 16:20:34 +0900

    feat: 로그인 요청 데이터 검증(Validation) 로직 추가

--- src/modules/auth/auth.validator.js
+++ src/modules/auth/auth.validator.js
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { email, z } from "zod";
 
 export const signupSchema = z.object({
   body: z.object({
@@ -9,5 +9,12 @@ export const signupSchema = z.object({
   }).refine((data) => data.password === data.passwordConfirm, {
     message: "비밀번호가 일치하지 않습니다.",
     path: ["passwordConfirm"],
+  }),
+});
+
+export const loginSchema = z.object({
+  body: z.object({
+    email: z.string().trim().toLowerCase().email("올바른 이메일 형식이 아닙니다."),
+    password: z.string().min(1, "비밀번호를 입력해 주세요."),
   }),
 });
