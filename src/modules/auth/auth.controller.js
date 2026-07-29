387cb76aea449d60a6d94483e7690db1e4325308
apple <apple@appleui-MacBookAir.local>
2026-07-29 12:00:32 +0900

    OAuth URL에 prompt=select_account 파라미터 추가

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -43,7 +43,7 @@ export async function logout(req, res) {
 export function googleLogin(req, res) {
   const redirectUri = process.env.GOOGLE_REDIRECT_URI;
   const clientId = process.env.GOOGLE_CLIENT_ID;
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=email profile`;
+  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=email profile&prompt=select_account`;
   
   res.redirect(url);
 }
